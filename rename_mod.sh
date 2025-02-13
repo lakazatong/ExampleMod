a32c47b5741bbb67de4e3deefcac713c4c1e6b10
lakazatong <Bo_wood@outlook.fr>
2025-02-13 22:45:06 +0100

    Update rename_mod.sh

--- rename_mod.sh
+++ rename_mod.sh
@@ -8,7 +8,6 @@ fi
 original_base="example_mod"
 replacement_base="$1"
 
-# Generate different case variations
 original_variants=(
     "example_mod"
     "example mod"
@@ -19,44 +18,35 @@ original_variants=(
 )
 
 replacement_variants=(
-    "$replacement_base"  # compact_circuits_mod
-    "$(echo "$replacement_base" | tr '_' ' ')"  # compact circuits mod
-    "$(echo "$replacement_base" | tr -d '_')"  # compactcircuitsmod
-    "$(echo "$replacement_base" | awk -F'_' '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2))}1' OFS='_')"  # Compact_Circuits_Mod
-    "$(echo "$replacement_base" | awk -F'_' '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2))}1' OFS=' ')"  # Compact Circuits Mod
-    "$(echo "$replacement_base" | awk -F'_' '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2))}1' OFS='')"  # CompactCircuitsMod
+    "$replacement_base"  # example_mod
+    "$(echo "$replacement_base" | tr '_' ' ')"  # example mod
+    "$(echo "$replacement_base" | tr -d '_')"  # examplemod
+    "$(echo "$replacement_base" | awk -F'_' '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2))}1' OFS='_')"  # Example_Mod
+    "$(echo "$replacement_base" | awk -F'_' '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2))}1' OFS=' ')"  # Example Mod
+    "$(echo "$replacement_base" | awk -F'_' '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2))}1' OFS='')"  # ExampleMod
 )
 
-echo "${replacement_variants[@]}"
-
-exit 0
-
-find . -depth -type d | while read path; do
-    newpath="$path"
-    for i in "${!original_variants[@]}"; do
-        newpath=$(echo "$newpath" | sed "s/${original_variants[$i]}/${replacement_variants[$i]}/g")
+for i in "${!original_variants[@]}"; do
+    find . -depth -type d -name "*${original_variants[$i]}*" ! -path "./.git*" | while read path; do
+        newpath=$(echo "$path" | sed "s/${original_variants[$i]}/${replacement_variants[$i]}/g")
+        [ "$path" != "$newpath" ] && mv "$path" "$newpath"
     done
-    [ "$path" != "$newpath" ] && mv -r "$path" "$newpath"
-done
 
-find . -depth -type f | while read path; do
-    newpath="$path"
-    for i in "${!original_variants[@]}"; do
-        newpath=$(echo "$newpath" | sed "s/${original_variants[$i]}/${replacement_variants[$i]}/g")
+    find . -depth -type f -name "*${original_variants[$i]}*" ! -path "./.git*" | while read path; do
+        newpath=$(echo "$path" | sed "s/${original_variants[$i]}/${replacement_variants[$i]}/g")
+        [ "$path" != "$newpath" ] && mv "$path" "$newpath"
     done
-    [ "$path" != "$newpath" ] && mv "$path" "$newpath"
-done
 
-grep -rl "${original_variants[0]}" . | while read file; do
-    for i in "${!original_variants[@]}"; do
+    grep -rl "${original_variants[$i]}" . --exclude-dir=".git" | while read file; do
         sed -i "s/${original_variants[$i]}/${replacement_variants[$i]}/g" "$file"
     done
 done
 
 current_dir=$(basename "$PWD")
 parent_dir=$(dirname "$PWD")
-new_root_name=$(echo "$current_dir" | tr '[:lower:]' '[:upper:]' | tr -d '_')
-
-cd "$parent_dir"
-mv "$current_dir" "$new_root_name"
-cd "$new_root_name"
+new_root_name="${replacement_variants[5]}"
+if [ -d "$parent_dir/$new_root_name" ]; then
+    echo "Error: $new_root_name already exists in the parent directory."
+    exit 1
+fi
+sudo mv "$parent_dir/$current_dir" "$parent_dir/$new_root_name"
