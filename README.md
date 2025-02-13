ff22527b092967bd92ad93ee9cb5e8818ad66b7b
lakazatong <Bo_wood@outlook.fr>
2025-02-13 23:19:21 +0100

    update rename mod script

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This template is for Minecraft 1.12.4+ and uses:
 - fabric 0.112.2
 - parchment 2024.12.22
 
-You can use `./rename_author.sh` and `./rename_mod.sh` to properly rename folders, files and files' content
+You can use `sudo ./rename_author.sh your_mod_name` and `sudo ./rename_mod.sh your_nickname` to properly rename folders, files and files' content
 
 # Credits
 
--- rename_mod.sh
+++ rename_mod.sh
@@ -1,45 +1,49 @@
 #!/bin/bash
 
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <new_mod_name (a_b_c format)>"
-    exit 1
+	echo "Usage: $0 <new_mod_name (a_b_c format)>"
+	exit 1
 fi
 
 original_base="example_mod"
 replacement_base="$1"
 
 original_variants=(
-    "example_mod"
-    "example mod"
-    "examplemod"
-    "Example_Mod"
-    "Example Mod"
-    "ExampleMod"
+	"example_mod"
+	"example mod"
+	"examplemod"
+	"Example_Mod"
+	"Example Mod"
+	"ExampleMod"
 )
 
 replacement_variants=(
-    "$replacement_base"  # example_mod
-    "$(echo "$replacement_base" | tr '_' ' ')"  # example mod
-    "$(echo "$replacement_base" | tr -d '_')"  # examplemod
-    "$(echo "$replacement_base" | awk -F'_' '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2))}1' OFS='_')"  # Example_Mod
-    "$(echo "$replacement_base" | awk -F'_' '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2))}1' OFS=' ')"  # Example Mod
-    "$(echo "$replacement_base" | awk -F'_' '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2))}1' OFS='')"  # ExampleMod
+	"$replacement_base"  # example_mod
+	"$(echo "$replacement_base" | tr '_' ' ')"  # example mod
+	"$(echo "$replacement_base" | tr -d '_')"  # examplemod
+	"$(echo "$replacement_base" | awk -F'_' '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2))}1' OFS='_')"  # Example_Mod
+	"$(echo "$replacement_base" | awk -F'_' '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2))}1' OFS=' ')"  # Example Mod
+	"$(echo "$replacement_base" | awk -F'_' '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2))}1' OFS='')"  # ExampleMod
 )
 
+rm -rf .vscode .idea
+./gradlew clean
+./gradlew --stop
+
 for i in "${!original_variants[@]}"; do
-    find . -depth -type d -name "*${original_variants[$i]}*" ! -path "./.git*" | while read path; do
-        newpath=$(echo "$path" | sed "s/${original_variants[$i]}/${replacement_variants[$i]}/g")
-        [ "$path" != "$newpath" ] && mv "$path" "$newpath"
-    done
+	find . -depth -type d -name "*${original_variants[$i]}*" ! -path "./.git*" | while read path; do
+		newpath=$(echo "$path" | sed "s/${original_variants[$i]}/${replacement_variants[$i]}/g")
+		[ "$path" != "$newpath" ] && mv "$path" "$newpath"
+	done
 
-    find . -depth -type f -name "*${original_variants[$i]}*" ! -path "./.git*" | while read path; do
-        newpath=$(echo "$path" | sed "s/${original_variants[$i]}/${replacement_variants[$i]}/g")
-        [ "$path" != "$newpath" ] && mv "$path" "$newpath"
-    done
+	find . -depth -type f -name "*${original_variants[$i]}*" ! -path "./.git*" | while read path; do
+		newpath=$(echo "$path" | sed "s/${original_variants[$i]}/${replacement_variants[$i]}/g")
+		[ "$path" != "$newpath" ] && mv "$path" "$newpath"
+	done
 
-    grep -rl "${original_variants[$i]}" . --exclude-dir=".git" | while read file; do
-        sed -i "s/${original_variants[$i]}/${replacement_variants[$i]}/g" "$file"
-    done
+	grep -rl "${original_variants[$i]}" . --exclude-dir=".git" | while read file; do
+		sed -i "s/${original_variants[$i]}/${replacement_variants[$i]}/g" "$file"
+	done
 done
 
-echo "Do not forget to rename the root folder to ${replacement_variants[5]}"
+[ "$(basename "$PWD")" != "${replacement_variants[5]}" ] && echo "Do not forget to rename the root folder to ${replacement_variants[5]}"
